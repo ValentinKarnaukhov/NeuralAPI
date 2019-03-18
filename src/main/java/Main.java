0ec08818796d569bd0d5daf65a8263fc5761003b
Karnaukhov Valentin <wippi2010@rambler.ru>
2019-03-18 20:07:04 +0300

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,11 +9,12 @@ public class Main {
 
         List<Layer> layerList=new LinkedList<>();
 
-        Layer inputLayer=new InputLayer(1);
+        Layer inputLayer=new InputLayer(2);
 
         Layer outputLayer=new HiddenLayer(1);
 
         layerList.add(inputLayer);
+        layerList.add(new HiddenLayer(4));
         layerList.add(new HiddenLayer(4));
         layerList.add(outputLayer);
 
@@ -21,7 +22,7 @@ public class Main {
         Network network=new Network(layerList);
 
 
-        DataSet trainingSet=new DataSet(1,1);
+        DataSet trainingSet=new DataSet(2,1);
         /*
         for(int i=0;i<10;i++){
             for(int j=0;j<10;j++){
@@ -30,23 +31,20 @@ public class Main {
         }
         */
 
-        trainingSet.add(new double[]{0.1},new double[]{0.1});
-        trainingSet.add(new double[]{0.2},new double[]{0.2});
-        trainingSet.add(new double[]{0.3},new double[]{0.3});
-        trainingSet.add(new double[]{0.4},new double[]{0.4});
-        trainingSet.add(new double[]{0.5},new double[]{0.5});
+        trainingSet.add(new double[]{0,0},new double[]{0});
+        trainingSet.add(new double[]{0,1},new double[]{1});
+        trainingSet.add(new double[]{1,0},new double[]{1});
+        trainingSet.add(new double[]{1,1},new double[]{0});
 
 
         for(int i=0;i<1000000;i++){
             network.learn(trainingSet);
         }
 
-        double[] testInput1=new double[]{0.15};
-        double[] testInput2=new double[]{0.25};
-        double[] testInput3=new double[]{0.35};
-        double[] testInput4=new double[]{0.45};
-        double[] testInput5=new double[]{0.55};
-        double[] testInput6=new double[]{0.65};
+        double[] testInput1=new double[]{0,0};
+        double[] testInput2=new double[]{0,1};
+        double[] testInput3=new double[]{1,0};
+        double[] testInput4=new double[]{1,1};
 
 
         network.setInput(testInput1);
@@ -65,13 +63,6 @@ public class Main {
         network.calculate();
         System.out.println(Arrays.toString(network.getOutput()));
 
-        network.setInput(testInput5);
-        network.calculate();
-        System.out.println(Arrays.toString(network.getOutput()));
-
-        network.setInput(testInput6);
-        network.calculate();
-        System.out.println(Arrays.toString(network.getOutput()));
 
     }
 
